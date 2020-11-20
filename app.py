94a275cdd8b2d0a5ba262160fe90a6a2a5265ba5
Rafael Montúfar <rafael@empleosti.com.mx>
2020-11-20 17:40:39 -0600

    First:commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask 
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/api/v1/users/<id>', methods=['GET'])
+def get_users(id):
+	response = {'message': 'success'}
+	return jsonify(response)
+
+@app.route('/api/v1/users/', methods=['POST'])
+def create_users():
+	response = {'message': 'Usuario creado con exito'}
+	return jsonify(response)
+
+@app.route('/api/v1/users/<id>', methods=['DELETE'])
+def delete_user(id):
+	response = {'message': 'Usuario eliminado con exito'}
+	return jsonify(response)
+
+if __name__ == '__main__':
+	app.run()
